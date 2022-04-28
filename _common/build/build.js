dd76b5a4580f637b1c567e42d45f3efbde7d88ab
Alexandr <samurayiii1986@gmail.com>
2022-04-28 18:31:21 +0300

    up

--- _common/build/build.js
+++ _common/build/build.js
@@ -2,7 +2,13 @@ const child_process = require("child_process");
 const path = require("path");
 const pkg = require(path.resolve(process.cwd(),`package.json`));
 
-const command = `docker build -t ${pkg.docker_image}:${pkg.version} .`;
+if (process.env["BUILD_IMAGE"] === undefined) {
+    console.error("Error: Environment 'BUILD_IMAGE' not set");
+    process.exit(1);
+}
+
+const docker_image = process.env["BUILD_IMAGE"];
+const command = `docker build -t ${docker_image}:${pkg.version} .`;
 
 console.log(`cwd:  ${__dirname}`);
 console.log(`exec:  ${command}`);
