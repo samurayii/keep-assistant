da665060a5a223c2d355da0abdcb48e75392c89f
Alexandr <samurayiii1986@gmail.com>
2022-08-09 16:35:14 +0300

    fix schema

--- src/lib/tools/ajv_error_helper.ts
+++ src/lib/tools/ajv_error_helper.ts
@@ -6,7 +6,11 @@ export function AjvErrorHelper (validate: ValidateFunction): string {
     let result = "";
 
     for (const item of validate.errors) {
-        result = `${result}  - Key ${chalk.yellow(item.instancePath.replace(/^\//, ""))} ${item.message}\n`;
+        result = `${result}  - Key ${chalk.yellow(item.instancePath.replace(/^\//, ""))} ${item.message}`;
+        if (item.keyword === "additionalProperties") {
+            result = `${result}. Key ${chalk.red(item.params?.["additionalProperty"])} superfluous`;
+        }
+        result = `${result}\n`;
     }
 
     return result;
